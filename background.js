3e59295ec6f00287b5ac0194035a91b6de04469e
nightpool <nightpool@cybre.space>
2026-03-06 13:40:35 -0800

    Clean up background.js

--- background.js
+++ background.js
@@ -1,21 +1,27 @@
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg.action !== 'startStandup') return;
-
-  chrome.tabs.create({ url: msg.url }, (tab) => {
-    chrome.tabs.onUpdated.addListener(function listener(tabId, info) {
-      if (tabId === tab.id && info.status === 'complete') {
+function waitForTabLoad(tabId) {
+  return new Promise((resolve) => {
+    chrome.tabs.onUpdated.addListener(function listener(id, info) {
+      if (id === tabId && info.status === 'complete') {
         chrome.tabs.onUpdated.removeListener(listener);
-        chrome.scripting.insertCSS({ target: { tabId: tab.id }, files: ['styles.css'] });
-        chrome.scripting.executeScript({
-          target: { tabId: tab.id },
-          files: ['content.js'],
-        }).then(() => {
-          chrome.tabs.sendMessage(tab.id, {
-            action: 'initStandup',
-            excludedUsers: msg.excludedUsers || [],
-          });
-        });
+        resolve();
       }
     });
   });
+}
+
+async function startStandup(msg) {
+  const standupTab = await chrome.tabs.create({ url: msg.url });
+  await waitForTabLoad(standupTab.id);
+  await chrome.scripting.insertCSS({ target: { tabId: standupTab.id }, files: ['styles.css'] });
+  await chrome.scripting.executeScript({ target: { tabId: standupTab.id }, files: ['content.js'] });
+  await chrome.tabs.sendMessage(standupTab.id, {
+    action: 'initStandup',
+    excludedUsers: msg.excludedUsers,
+  });
+}
+
+chrome.runtime.onMessage.addListener((msg) => {
+  if (msg.action === 'startStandup') {
+    startStandup(msg);
+  }
 });
